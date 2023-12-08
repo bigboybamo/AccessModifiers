24694183202be68c2dd0e319bc9a78c6de3d7c04
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2023-12-08 23:02:55 +0100

    first commit

---
+++ AccesModTutorial/ExampleClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesModTutorial
+{
+    public class ExampleClass
+    {
+        public string? PublicName { get; set; }
+        private string PrivateName { get; set; }
+        protected string? ProtectedName { get; set; }
+        private protected string? ProtectedPrivateName { get; set;}
+
+        internal string? InternalName { get; set; }
+
+        public ExampleClass()
+        {
+            PrivateName = "Test";
+        }
+
+        public string GetPrivateName()
+        {
+            return PrivateName;
+        }
+
+    }
+
+    class ChlidExampleClass : ExampleClass
+    {
+        public ChlidExampleClass()
+        {
+                ProtectedName = "Test";
+                ProtectedPrivateName = "Test";
+        }
+        public string? GetPrivateProtectedName()
+        {
+            return ProtectedName;
+        }
+
+        public string? GetProtectedName()
+        {
+            return ProtectedPrivateName;
+        }
+    }
+}
---
+++ AccesModTutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AccesModTutorial;
+
+ExampleClass exampleClass = new ExampleClass();
+exampleClass.PublicName = "Test";
+exampleClass.GetPrivateName();
+ChlidExampleClass chlidExampleClass = new ChlidExampleClass();
+chlidExampleClass.GetProtectedName();
+chlidExampleClass.GetPrivateProtectedName();
+exampleClass.InternalName = "Test";
+Console.WriteLine(exampleClass.PublicName);
+Console.WriteLine(exampleClass.GetPrivateName());
+Console.WriteLine(chlidExampleClass.GetProtectedName());
+Console.WriteLine(chlidExampleClass.GetPrivateProtectedName());
+Console.WriteLine(exampleClass.InternalName);
